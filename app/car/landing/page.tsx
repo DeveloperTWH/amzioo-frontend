d463a4968245868d82a3b48780298ce23508279b
Sk Faiz Rahaman <skfaizrahaman@outlook.com>
2026-04-24 19:58:03 +0530

    car rental design

---
+++ app/car/landing/page.tsx
@@ -0,0 +1,41 @@
+import { HeaderBar } from "@/app/componenents/header/header"
+import { FlightSearchPanel} from "@/app/componenents/flight/searchPanel"
+import { TopDestination } from "@/app/componenents/flight/topDestination"
+import { SpecialOffer } from "@/app/componenents/home/specialoffer"
+
+import BookFlightSection from "@/app/componenents/flight/bookFlightSection"
+
+
+export default function CarLandingPage() {
+
+
+
+    return(
+           <div>
+              <div
+                className="min-h-[906px] bg-cover bg-center bg-no-repeat"
+                style={{ backgroundImage: "url('/car rental.png')" }}
+              >
+              <HeaderBar/>
+              <div className="flex flex-col justify-center items-center h-[50%]">
+                <p className="font-bold text-white font-[Vollkorn] text-[60px]">Car Rentals for Every Journey</p>
+                <div className="font-medium text-white font-[Monserrat] text-center  whitespace-pre-line">{`Search, compare, and book flights at the best prices. Enjoy seamless booking,\nflexible options, and instant confirmations.`}</div>
+        
+              </div>
+              <div className="mt-40 w-full">
+                <FlightSearchPanel/>
+              </div>
+              </div>
+        
+              <div className="mt-20">
+                <TopDestination/>
+              </div>
+        
+              <div className="mt-20">
+                <SpecialOffer/>
+              </div>
+        
+              <BookFlightSection/>
+            </div>
+    )
+}
